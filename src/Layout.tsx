a1947e38c27622f223ff9f87e8144ac0c7e79e6f
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-07-11 07:15:08 -0700

    import assets, configure overall layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,9 +2,34 @@ import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
   return (
-    <div>
-      Space Travel
+    <>
+      <header>
+        <h1>
+          <a href="/index">Space Travel</a>
+        </h1>
+        <nav id="siteNav">
+          <input id="hamburgerToggle" type="checkbox" hidden />
+          <label htmlFor="hamburgerToggle" id="icon"></label>
+          <div id="wrapperForMobileSidebar">
+            <ol>
+              <li className="active">
+                <a href="/index">Home</a>
+              </li>
+              <li>
+                <a href="/destination">Destination</a>
+              </li>
+              <li>
+                <a href="/crew">Crew</a>
+              </li>
+              <li>
+                <a href="/tech">Technology</a>
+              </li>
+            </ol>
+          </div>
+        </nav>
+      </header>
+
       <Outlet />
-    </div>
+    </>
   )
 }
