4ea750a2713175418a6f43bef48326e9ed95d8d1
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-07-11 07:25:56 -0700

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Layout from './Layout.tsx'
 import Home from './Home.tsx'
 import './styles/screen.css'
+import { useLayoutEffect } from 'react'
 
 function App() {
+  const location = useLocation()
+
+  useLayoutEffect(() => {
+    switch (location.pathname) {
+      case '/':
+        document.body.id = 'homePage'
+        break
+      case '/destination':
+        document.body.id = 'destinationPage'
+        break
+      case '/crew':
+        document.body.id = 'crewPage'
+        break
+      case '/tech':
+        document.body.id = 'techPage'
+        break
+    }
+  }, [location.pathname])
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-      </Route>
-    </Routes>
+    <div id="container">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,23 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <main>
+      <div id="contentWrapper">
+        <div id="content">
+          <small id="pageTitle">So, you want to travel to</small>
+
+          <h2>
+            Space
+          </h2>
+
+          <p>Prepare to embark on an authentic journey into outer space, as we invite you to immerse yourself
+            in an unparalleled cosmic experience. Please, take a moment to recline and anticipate the extraordinary
+            adventure that awaits you.</p>
+        </div>
+      </div>
+
+      <div id="buttonWrapper">
+        <a href="/destination" className="button">Explore</a>
+      </div>
+    </main>
   )
 }
